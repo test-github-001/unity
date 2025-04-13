070273a9ddb172b2d3a5f504b2e7d67a6ac36096
test-github-001 <107862407+test-github-001@users.noreply.github.com>
2025-04-13 12:42:19 +0300

    Create PlayerGun.cs

---
+++ PlayerGun.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGun : MonoBehaviour
+{
+    public GameObject bulletPrefab;     // Префаб пули
+    public Transform firePoint;         // Точка появления пули
+    public float bulletSpeed = 10f;     // Скорость пули
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        firePoint = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.RightControl))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+
+        // Направление в зависимости от флипа спрайта
+        float direction = spriteRenderer.flipX ? -1f : 1f;
+
+        Bullet bulletScript = bullet.GetComponent<Bullet>();
+        if (bulletScript != null)
+        {
+            bulletScript.SetDirection(direction);
+        }
+
+    }
+}
