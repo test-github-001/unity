1e05caf04a61e05326b07c63725621fd58c4344f
test-github-001 <107862407+test-github-001@users.noreply.github.com>
2025-11-08 16:11:49 +0300

    Update EnemiesSpawner.cs

--- space/EnemiesSpawner.cs
+++ space/EnemiesSpawner.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject enemyPrefab;     // Префаб пули
+    public GameObject enemyPrefab;
+    public GameObject enemyDiscPrefab;
 
     public float addEnemyMinTime = 1f;
     public float addEnemyMaxTime = 2f;
 
+    public float addEnemyDiscMinTime = 5f;
+    public float addEnemyDiscMaxTime = 10f;
+
     private float addEnemyTimeout = 1f;
+    private float addEnemyDiscTimeout = 1f;
 
     void Start()
     {
         addEnemyTimeout = Random.Range(addEnemyMinTime, addEnemyMaxTime);
+        addEnemyDiscTimeout = Random.Range(addEnemyDiscMinTime, addEnemyDiscMaxTime);
     }
 
     // Update is called once per frame
@@ -25,10 +31,22 @@ public class EnemySpawner : MonoBehaviour
             addEnemyTimeout = Random.Range(addEnemyMinTime, addEnemyMaxTime);
             AddEnemy();
         }
+
+        addEnemyDiscTimeout -= Time.deltaTime;
+        if (addEnemyDiscTimeout < 0f)
+        {
+            addEnemyDiscTimeout = Random.Range(addEnemyDiscMinTime, addEnemyDiscMaxTime);
+            AddEnemyDisc();
+        }
     }
 
     void AddEnemy()
     {
         GameObject enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+    }
+
+    void AddEnemyDisc()
+    {
+        GameObject enemy = Instantiate(enemyDiscPrefab, transform.position, Quaternion.identity);
     }
 }
